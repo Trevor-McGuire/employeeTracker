3f4c0991b42bfeb1bfd137e2f4e2a23d270b4701
Trevor-McGuire <trevor.mcguire.candidate@gmail.com>
2023-07-04 15:26:32 -0500

    init inquire

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
 const inquirer = require('inquirer')
 const fs = require('fs')
-const licenseArray = ["MIT License","Apache License","GPL v2 License","GPL v3 License","BSD 3 License","BSD 2 License",]
-const licenseBdgArray = [
-  "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
-  "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
-  "[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)",
-  "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)",
-  "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)",
-  "[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)",
-]
 
 inquirer
   .prompt([
