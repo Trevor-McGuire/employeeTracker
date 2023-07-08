0e67086bebbc53b7b2b423360b85be1e810386bf
Trevor-McGuire <trevor.mcguire.candidate@gmail.com>
2023-07-08 16:49:56 -0500

    completed add employee and working on updating role

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const inquirer = require('inquirer')
 const fs = require('fs')
 const mysql = require('mysql');
 
+const { v4: uuidv4 } = require('uuid');
+uuidv4()
+
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
@@ -61,6 +64,7 @@ const startInquire = () => {
   })
 }
 startInquire()
+
 // select, order, and print all employee last and first names to console
 const viewAllEmployees = () => {
   connection.query('SELECT * FROM employee ORDER BY last_name', (err, rows) => {
@@ -68,29 +72,59 @@ const viewAllEmployees = () => {
     startInquire()
   });
 }
-const AddEmployee = () => {
-  console.info("AddEmployee()")
-  connection.connect((err) => {
-    if (err) throw err;
-    console.log('Connected!');
-    connection.query('SELECT * FROM employee', (err, rows) => {
+const AddEmployee = async () => {
+  await inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'first_name',
+      message: 'What is the employee\'s first name?',
+    },
+    {
+      type: 'input',
+      name: 'last_name',
+      message: 'What is the employee\'s last name?',
+    },
+    {
+      type: 'input',
+      name: 'role_id',
+      message: 'What is their role?',
+    },
+    {
+      type: 'input',
+      name: 'manager_id',
+      message: 'Who is their manager?',
+    },
+  ])
+  .then((response) => {
+    var sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('${response.first_name}', '${response.last_name}', '${response.role_id}', '${response.manager_id}')`;
+    connection.query(sql, function (err, result) {
       if (err) throw err;
-      console.log('Data received from Db:\n');
-      console.log(rows);
+      console.log("1 record inserted");
     });
-  });
+    viewAllEmployees()
+  })
+  
 }
 const UpdateEmployeeWorkRole = () => {
-  console.info("UpdateEmployeeWorkRole()")
-  connection.connect((err) => {
-    if (err) throw err;
-    console.log('Connected!');
-    connection.query('SELECT * FROM employee', (err, rows) => {
-      if (err) throw err;
-      console.log('Data received from Db:\n');
-      console.log(rows);
-    });
-  });
+  connection.query('SELECT * FROM employee ORDER BY last_name', (err, rows) => {
+    const employeeArray = []
+    rows.forEach(e => employeeArray.push(`${e.last_name}, ${e.first_name}`));
+    console.log(employeeArray)
+    inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'employee',
+        message: 'What employee needs their work role changed?',
+        choices: employeeArray
+      },
+    ])
+    .then((response) => {
+      console.log(response.employee,"asdf")
+    })
+    // startInquire()
+  })
 }
 const ViewAllWorkRoles = () => {
   connection.query('SELECT * FROM work_role ORDER BY title', (err, rows) => {
